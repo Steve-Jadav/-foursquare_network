efce7d07f882f714a67f4c73a4afc6d9b805a00d
Sagar Shah <sagarshah1@yahoo.com>
2019-09-25 16:35:39 -0700

    diameter

--- graph.py
+++ graph.py
@@ -3,8 +3,20 @@ import collections
 
 from typing import List
 from matplotlib import pyplot
+from plotly import express
+from pandas import DataFrame
 
 from data import collect, getname
+
+
+def write_to_file(graph: networkx.graph, filename: str):
+    node_link_data = networkx.readwrite.node_link_data(graph)
+    with open(filename, 'w') as f:
+        f.write(node_link_data)
+
+
+def read_from_file(filename: str):
+    pass
 
 
 def create(nodes: List[dict], edges: List[tuple]):
@@ -16,43 +28,26 @@ def create(nodes: List[dict], edges: List[tuple]):
 
 def draw_network(graph: networkx.Graph):
     # pyplot.subplot(121)
-    networkx.draw_random(graph, with_labels=True, font_weight='bold')
     labels = {userid: getname(user) for userid, user in graph.nodes(data=True)}
     networkx.relabel_nodes(graph, labels)
+    networkx.draw_random(graph, with_labels=True, font_weight='bold')
     pyplot.show()
 
 
 def draw_degree_histogram(graph: networkx.Graph):
     degree_sequence = sorted([d for n, d in graph.degree()], reverse=True)
-    degreeCount = collections.Counter(degree_sequence)
-    deg, cnt = zip(*degreeCount.items())
-
-    fig, ax = pyplot.subplots()
-    pyplot.bar(deg, cnt, width=0.80, color='b')
-
-    pyplot.title("Degree Histogram")
-    pyplot.ylabel("Count")
-    pyplot.xlabel("Degree")
-    ax.set_xticks([d + 0.4 for d in deg])
-    ax.set_xticklabels(deg)
-
-    # draw graph in inset
-    pyplot.axes([0.4, 0.4, 0.5, 0.5])
-    Gcc = sorted(networkx.connected_component_subgraphs(graph), key=len, reverse=True)[0]
-    pos = networkx.spring_layout(graph)
-    pyplot.axis('off')
-    networkx.draw_networkx_nodes(graph, pos, node_size=20)
-    networkx.draw_networkx_edges(graph, pos, alpha=0.4)
-
-    pyplot.show()
+    df = DataFrame(data={'degree': degree_sequence})
+    fig = express.histogram(df, x="degree")
+    fig.show()
 
 
-def calculate_measures(graph):
+def calculate_diameter(graph):
     pass
 
 
 if __name__ == '__main__':
     nodes, edges = collect(123455)
     graph = create(nodes, edges)
-    draw_network(graph)
-    draw_degree_histogram(graph)
+    write_to_file(graph, 'data.json')
+    # draw_network(graph)
+    # draw_degree_histogram(graph)
